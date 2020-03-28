b6281d4909ddba3e525be98b769f3f1a96c9713b
embokoir <30351534+embokoir@users.noreply.github.com>
2020-03-28 16:58:18 +0900

    add files

---
+++ line.js
@@ -0,0 +1,24 @@
+const axios = require('axios')
+const dotenv = require('dotenv')
+dotenv.config()
+
+const LINE_ROOM_TOKEN = process.env.LINE_ROOM_TOKEN
+async function notify(text) {
+  let params = new URLSearchParams()
+  params.append('message', text)
+  return axios({
+    url: 'https://notify-api.line.me/api/notify',
+    headers: {
+      "Authorization": `Bearer ${LINE_ROOM_TOKEN}`,
+      "Content-Type": 'application/x-www-form-urlencoded'
+    },
+    method: 'POST',
+    data: params
+  })
+    .then(res => Promise.resolve('Successfully Notified'))
+    .catch(err => {
+      console.log(err.response.data)
+      Promise.reject('Notify error')
+    })
+}
+module.exports.notify = notify
---
+++ server.js
@@ -0,0 +1,54 @@
+const puppeteer = require('puppeteer')
+const fs = require('fs').promises
+const line = require('./line');
+
+(async () => {
+  const url = 'https://cocorolife.jp.sharp/mask/'
+
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox']
+  })
+  const page = await browser.newPage()
+  await page.setViewport({ width: 1440, height: 1000 })
+  await page.setExtraHTTPHeaders({
+    'Accept-Language': 'ja'
+  })
+  await page.goto(url, { waitUntil: 'domcontentloaded' })
+
+  const prevValue1 = await fs.readFile('value1.txt', 'utf8', (err, data) => {
+    if (err) console.log(err)
+    return data
+  })
+  const prevValue2 = await fs.readFile('value2.txt', 'utf8', (err, data) => {
+    if (err) console.log(err)
+    return data
+  })
+
+  const value1 = await page.$eval('#top1 > div > main > article > div.text', elm => elm.textContent.trim())
+        .catch(async err => {
+          console.log(err)
+          process.exit(1)
+        })
+
+  const value2 = await page.$eval('#top1 > div > main > article > div:nth-child(4) > span:nth-child(1)', elm => elm.textContent.trim())
+        .catch(async err => {
+          console.log(err)
+          process.exit(1)
+        })
+
+  if (!(value1 == prevValue1 && value2 === prevValue2)) {
+    line.notify(`something changed!\ncheck ${url}`)
+  }
+  await fs.writeFile('value1.txt', new Uint8Array(Buffer.from(value1)), 'utf8', err => {
+    if (err) throw err
+    console.log('new value1 set')
+  })
+  await fs.writeFile('value2.txt', new Uint8Array(Buffer.from(value2)), 'utf8', err => {
+    if (err) throw err
+    console.log('new value2 set')
+  })
+
+  // Close
+  await browser.close()
+})()
